01669867f7f96dd09343fe49b0235dc49e31838f
Dipesh Trikam <dipesht@datacom.co.nz>
2018-05-19 14:43:32 +1200

    Initial Commit

---
+++ CameraCompleted/Tabs/CustomVision.xaml.cs
@@ -0,0 +1,96 @@
+﻿using Newtonsoft.Json;
+using Plugin.Media;
+using Plugin.Media.Abstractions;
+using System;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using Tabs.Model;
+using Xamarin.Forms;
+
+namespace Tabs
+{
+    public partial class CustomVision : ContentPage
+    {
+        public CustomVision()
+        {
+            InitializeComponent();
+        }
+
+		private async void loadCamera(object sender, EventArgs e)
+        {
+            await CrossMedia.Current.Initialize();
+
+            if (!CrossMedia.Current.IsCameraAvailable || !CrossMedia.Current.IsTakePhotoSupported)
+            {
+                await DisplayAlert("No Camera", ":( No camera available.", "OK");
+                return;
+            }
+
+            MediaFile file = await CrossMedia.Current.TakePhotoAsync(new StoreCameraMediaOptions
+            {
+                PhotoSize = PhotoSize.Medium,
+                Directory = "Sample",
+                Name = $"{DateTime.UtcNow}.jpg"
+            });
+
+            if (file == null)
+                return;
+
+            image.Source = ImageSource.FromStream(() =>
+            {
+                return file.GetStream();
+            });
+
+
+            await MakePredictionRequest(file);
+        }
+
+        static byte[] GetImageAsByteArray(MediaFile file)
+        {
+            var stream = file.GetStream();
+            BinaryReader binaryReader = new BinaryReader(stream);
+            return binaryReader.ReadBytes((int)stream.Length);
+        }
+
+        async Task MakePredictionRequest(MediaFile file)
+        {
+            var client = new HttpClient();
+
+			client.DefaultRequestHeaders.Add("Prediction-Key", "ad50d3fc9c2d401699e38851d71d7ef3");
+
+			string url = "https://southcentralus.api.cognitive.microsoft.com/customvision/v2.0/Prediction/9a692ab6-f269-4cd6-acfb-41137b1faec6/image?iterationId=09b1f692-2e6b-41d1-a65e-deb6b12183a4";
+
+            HttpResponseMessage response;
+
+            byte[] byteData = GetImageAsByteArray(file);
+
+            using (var content = new ByteArrayContent(byteData))
+            {
+
+                content.Headers.ContentType = new MediaTypeHeaderValue("application/octet-stream");
+                response = await client.PostAsync(url, content);
+
+
+                if (response.IsSuccessStatusCode)
+                {
+                    var responseString = await response.Content.ReadAsStringAsync();
+
+                    EvaluationModel responseModel = JsonConvert.DeserializeObject<EvaluationModel>(responseString);
+
+                    double max = responseModel.Predictions.Max(m => m.Probability);
+
+                    TagLabel.Text = (max >= 0.5) ? "Captain James Cook Located" : "No Land Mark Found";
+
+                }
+
+                //Get rid of file once we have finished using it
+                file.Dispose();
+            }
+        }
+
+    }
+}

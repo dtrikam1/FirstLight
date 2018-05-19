a534aef4f42124f4c8260fd91766e403418f146f
Dipesh Trikam <dipesht@datacom.co.nz>
2018-05-19 16:14:08 +1200

    Added GPS location with street address

--- CameraCompleted/Tabs/AzureTable.xaml.cs
+++ CameraCompleted/Tabs/AzureTable.xaml.cs
@@ -3,23 +3,36 @@ using System.Collections.Generic;
 using Microsoft.WindowsAzure.MobileServices;
 using Tabs.DataModels;
 using Xamarin.Forms;
+using Xamarin.Forms.Maps;
 
 namespace Tabs
 {
     public partial class AzureTable : ContentPage
     {
 		MobileServiceClient client = AzureManager.AzureManagerInstance.AzureClient;
+		Geocoder geoCoder;
 
         public AzureTable()
         {
             InitializeComponent();
+			geoCoder = new Geocoder();
         }
 
 		async void Handle_ClickedAsync(object sender, System.EventArgs e)
         {
-            List<FirstLightLandMarkInformation> notHotDogInformation = await AzureManager.AzureManagerInstance.GetFirstLightLandMarkInformation();
+            loading.IsRunning = true;
+			List<FirstLightLandMarkInformation> firstLightLandMarkInformation = await AzureManager.AzureManagerInstance.GetFirstLightLandMarkInformation();
 
-			FirstLightLandMarkInformationList.ItemsSource = notHotDogInformation;
+			foreach (FirstLightLandMarkInformation model in firstLightLandMarkInformation)
+            {
+                var position = new Position(model.Latitude, model.Longitude);
+                var possibleAddresses = await geoCoder.GetAddressesForPositionAsync(position);
+                foreach (var address in possibleAddresses)
+                    model.City = address;
+            }
+
+			FirstLightLandMarkInformationList.ItemsSource = firstLightLandMarkInformation;
+            loading.IsRunning = false;
         }
     }
 }
--- CameraCompleted/iOS/AppDelegate.cs
+++ CameraCompleted/iOS/AppDelegate.cs
@@ -12,7 +12,9 @@ namespace Tabs.iOS
     {
         public override bool FinishedLaunching(UIApplication app, NSDictionary options)
         {
-            global::Xamarin.Forms.Forms.Init();
+			global::Xamarin.Forms.Forms.Init();
+                     
+            Xamarin.FormsMaps.Init();
 
             // Code for starting up the Xamarin Test Cloud Agent
 #if DEBUG
